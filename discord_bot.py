2b7d57d406f390c054e32752db9d98146f6fcfd4
Camila Martinez <camilamarb98@gmail.com>
2022-07-21 20:20:52 -0500

    implement google api

--- discord_bot.py
+++ discord_bot.py
@@ -1,25 +1,20 @@
 from config import *
+from google_search import search_image_links
+
 import random
-import json
 
 import discord
 from discord.ext import commands
 
-# READ JSON FILE
-with open("posts_url.json") as f:
-    urls = json.loads(f.read())
+def create_bot():
+    # DISCORD
+    bot = commands.Bot(command_prefix='!')
+    client = discord.Client()
 
-# DISCORD
-bot = commands.Bot(command_prefix='!')
-client = discord.Client()
+    @bot.command(name='dua')
+    async def photos(ctx):
+        await ctx.send(search_image_links())
 
-@bot.command(name='dua')
-async def photos(ctx):
-    # for downloaded pictures
-    # pic = str(random.choice(os.listdir("dualipa")))
-    # await ctx.send(file=discord.File('dualipa/'+pic))
+    bot.run(TOKEN)
 
-    await ctx.send(random.choice(urls))
-
-
-bot.run(TOKEN)
+create_bot()
--- google_search.py
+++ google_search.py
@@ -1,10 +1,14 @@
 import requests
-from config import *
-link = f"https://customsearch.googleapis.com/customsearch/v1?cx={CX}&imgType=photo&num=1&q=dua%20lipa&searchType=image&start=1&prettyPrint=true&key={API}"
+from config import CX, API
 
-response = requests.get(link)
-json_dict = response.json()
 # print(json_dict)
 
-for image in json_dict["items"]:
-    print(image.get('link'))
+def search_image_links():
+    link = f"https://customsearch.googleapis.com/customsearch/v1?cx={CX}&imgType=photo&num=1&q=dua%20lipa&searchType=image&start=1&prettyPrint=true&key={API}"
+
+    response = requests.get(link)
+    json_dict = response.json()
+
+    for image in json_dict["items"]:
+        return image.get('link')
+ 
