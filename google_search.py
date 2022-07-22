15687ae035051cf7dbed0029017d89e134128531
cjmazzarri <cjmazzarri@gmail.com>
2022-07-21 23:15:42 -0500

    ✨Added array size as a variable

--- google_search.py
+++ google_search.py
@@ -3,9 +3,10 @@ import requests
 from config import CX, API
 
 # print(json_dict)
+arraySize = 50
 
 def search_image_links():
-    link = f"https://customsearch.googleapis.com/customsearch/v1?cx={CX}&imgType=photo&num=3&q=dua%20lipa&searchType=image&start=1&prettyPrint=true&key={API}"
+    link = f"https://customsearch.googleapis.com/customsearch/v1?cx={CX}&imgType=photo&{arraySize}&q=dua%20lipa&searchType=image&start=1&prettyPrint=true&key={API}"
 
     response = requests.get(link)
     json_dict = response.json()
@@ -14,6 +15,7 @@ def search_image_links():
 
     for image in json_dict["items"]:
         links_arrays.append(image.get('link'))
+
 
     return links_arrays
 
