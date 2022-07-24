62f70f36887003018c16ad0e45da9de8c12593ff
Camila Martinez <camilamarb98@gmail.com>
2022-07-24 11:24:17 -0500

    add event on ready | add optional argument to dua command

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -1,3 +1,4 @@
+from os import name
 from .config import *
 from .google_search import search_image_links
 
@@ -6,14 +7,21 @@ import random
 import discord
 from discord.ext import commands
 
+def my_test():
+    return 'my message'
+
 def create_bot():
     # DISCORD
     bot = commands.Bot(command_prefix='!')
     client = discord.Client()
 
-    @bot.command(name='dua')
-    async def photos(ctx):
-        await ctx.send(search_image_links())
+    @bot.event
+    async def on_ready():
+        await bot.change_presence(activity=discord.Streaming(name="Levitating", url="https://www.youtube.com/watch?v=TUVcZfQe-Kw"))
+        print("Dua Lipa Bot is ready.")
 
-    print("running...")
+    @bot.command(name='dua')
+    async def photos(ctx, *, extra:str):
+        await ctx.send(search_image_links(extra))
+
     bot.run(TOKEN)
