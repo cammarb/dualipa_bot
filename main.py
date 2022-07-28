cab4c5b1d486c682834f9d91a14fbf1bcc67e73d
Camila Martinez <camilamarb98@gmail.com>
2022-07-27 19:40:29 -0500

    update main file structure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import asyncio
 import os
+import platform
+import asyncio
+
 import discord
 import config
 from discord.ext import commands
@@ -7,21 +9,34 @@ from discord import app_commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents, application_id=config.APP_ID)
+
+bot = commands.Bot(
+    command_prefix="!", 
+    intents=intents, 
+    application_id=config.APP_ID,
+    )
 
 @bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name="Levitating", url="https://www.youtube.com/watch?v=TUVcZfQe-Kw"))
+async def on_ready() -> None:
+    await bot.change_presence(activity=discord.Game(name="Levitating 🚀"))
+    print(f"Python version: {platform.python_version()}")
+    print(f"Discord API version: {discord.__version__}")
     print("Bot is ready.")
 
-async def load():
-    for file in os.listdir('./cogs'):
+async def load_commands(command_type: str) -> None:
+    for file in os.listdir(f'./cogs/{command_type}'):
         if file.endswith('.py'):
-            await bot.load_extension(f'cogs.{file[:-3]}')
-
+            extension = file[:-3]
+        try:
+            await bot.load_extension(f"cogs.{command_type}.{extension}")
+            print(f"Loaded extension '{extension}'")
+        except Exception as e:
+            exception = f"{type(e).__name__}: {e}"
+            print(f"Failed to load extension {extension}\n{exception}")
 
 async def main():
-    await load()
+    await load_commands("normal")
     await bot.start(config.TOKEN)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
